4e33cadb5166868cfcb75aa138742ff52a35af1b
Namkat Cedrick <namkatcedrickjumtock3@gmail.com>
2022-08-20 12:24:53 +0100

    started js to dart overview

--- App/server/bin/builtintypes.dart
+++ App/server/bin/builtintypes.dart
@@ -1,9 +1,25 @@
+import 'dart:io';
+
+Map env = Platform.environment;
 void main() {
   num a = 2;
   num b = 5.45;
   double heigh = 40.23;
 
-  print("I am ${heigh.round()}");
+  // print("I am ${heigh.round()}");
   // returns a boolean
-  print(identical(a, b));
+  // print(identical(a, b));
+
+  // check plateform info
+  // print("We run this from this Vm:${Platform.executable}");
+
+  // another optional params could be
+  print("ours os type is ${Platform.isLinux}"); //returns a boolean
+  // print("our os is:${Platform.operatingSystem}");
+  // print("dart version:${Platform.version}");
+}
+
+// dart functions are strongely typ
+int multiply(int a, int b) {
+  return a * b;
 }
--- ReadMe.md
+++ ReadMe.md
@@ -1,8 +1,18 @@
 ##### Dart Programing notes
-
+ To indicate that a variable might have the value null, just add ? to its type declaration: and ! makes sure the value is not nullable
 ```
-    To indicate that a variable might have the value null, just add ? to its type declaration: and ! makes sure the value is not nullable
+   
         int? value =; 5
+        
 ```
 
-#### Classes
+##### Dart possitional functions with optional params
+
+optional values are placed in a square brackets: meaning if the args are not
+passed when the function is called no run time error is shown
+
+```
+  multiply(int a, [int b = 5, int? c]) {
+  ...
+}
+``` 
